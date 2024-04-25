e6cea4f82a4a6bb4dc2a5b47cee4d2e38d370049
Dzonex <nikola2006.novakovic@gmail.com>
2024-04-25 18:56:23 +0200

    Finished project

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ private:
     int front = -1, rear = -1;
     int capacity = 0, count = 0;
 public:
-    Queue(int capacity){
+    Queue(const int capacity){
         arr = new int[capacity];
         front = 0, rear = 0;
         this->capacity = capacity;
@@ -16,7 +16,7 @@ public:
         delete[] arr;
         arr = nullptr;
     }
-    void enqueue(int element){
+    void enqueue(const int element){
         // If the array is full
         if(isFull()){
             cout << "Array is full!" << endl;
@@ -47,77 +47,120 @@ public:
         front = (front + 1) % capacity;
         count--;
     }
-    void full_print(){
+    void print(){
+        // Checks if the queue is empty
         if(isEmpty()){
             cout << "The queue is empty!" << endl;
             return;
         }
-        for(int i = rear; i >= front; i--){
-            if(i == front){
-              cout << arr[i] << endl;
-              return;
-            }
+        // Prints the elements of the array
+        cout << "Queue: ";
+        for(int i = rear; i > front; i--){
             cout << arr[i] << "->";
         }
+        cout << arr[front] << endl;
     }
-    int peek_front(){
+    void full_print(){
+        // Prints everything about the queue
+        cout << "All values for a queue:" << endl;
+        cout << "Capacity: " << max_size() << ", size: " << size() << endl;
+        cout << std::boolalpha << "Empty: " << isEmpty() << ", Full: " << isFull() << std::noboolalpha << endl;
+        print();
+    }
+    int peek_front() const{
+        // Returns the front element otherwise returns default value of front
         if(!isEmpty()){
             return arr[front];
         }
         return -1;
     }
-    int peek_back(){
+    int peek_back() const{
+        // Returns the rear element otherwise returns default value of rear
         if(!isEmpty()){
             return arr[rear];
         }
         return -1;
     }
-    bool isFull(){
+    bool isFull() const{
         // If the rear == front then it's full
         if(count == capacity){
             return true;
         }
         return false;
     }
-    bool isEmpty(){
+    bool isEmpty() const{
         // If the count is 0 then it's empty
         if(count == 0){
             return true;
         }
         return false;
     }
-    void fill(){
-        for(int i = 0; i < capacity; i++){
-            enqueue(i);
-        }
-    }
-    int size(){
+    int size() const{
+        // Returns the size of the array
         return count;
     }
-    void getFront(){
-      cout << front << endl;
+    int max_size() const{
+        // Returns the capacity of the array
+        return capacity;
+    }
+    void fill(){
+        if(isFull()){
+            cout << "Queue is full" << endl;
+            return;
+        }
+        // Fills the queue with 0
+        const int remainingSpace = capacity - count;
+        for(int i = 0; i < remainingSpace; i++){
+            enqueue(0);
+        }
+    }
+    void fill(const int element){
+        if(isFull()){
+            cout << "Queue is full" << endl;
+            return;
+        }
+        // Fills the array with a specified element
+        const int remainingSpace = capacity - count;
+        for(int i = 0; i < remainingSpace; i++){
+            enqueue(element);
+        }
+    }
+    void fill(const int element, const int len){
+        if(isFull()){
+            cout << "Queue is full" << endl;
+            return;
+        }
+        // Fills the array with a specified element up to a certain length
+        const int remainingSpace = capacity - count;
+        if(remainingSpace < len){
+            cout << "Cannot fill up to that length" << endl;
+            return;
+        }
+        for(int i = 0; i < len; i++){
+            enqueue(element);
+        }
+        return;
+    }
+    void clear(){
+        // Empties the queue
+        if(isEmpty()){
+            return;
+        }
+        while(count != 0){
+            dequeue();
+        }
     }
 };
 
 int main(){
-    Queue queue(10);
+    Queue queue(10);    
 
-    /*queue.enqueue(20);
-    queue.enqueue(10);
-    queue.enqueue(15);
-    queue.dequeue();
-    queue.enqueue(50);
-    queue.enqueue(571);*/
-    queue.fill();
-    cout << "Front: ";                   queue.getFront();
-    queue.dequeue();
-    queue.dequeue();
-    cout << "Front: ";
-    queue.getFront();
+    for(int i = 0; i < 6; i++){
+        queue.enqueue(i*i);
+    }
+    queue.enqueue(7438);
+    queue.fill(5, 2);
     queue.full_print();
-    cout << endl;
-    cout << "Empty: " << queue.isEmpty() << ", Full: " << queue.isFull() << endl;
-    cout << "Front: " << queue.peek_front() << ", Back: " << queue.peek_back();
 
     return 0;
 }
